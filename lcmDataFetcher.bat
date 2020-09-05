ba293fb3b17794c36c042a013fbe480e218c1198
Reto Marek <reto.marek@hslu.ch>
2020-09-05 03:21:18 +0200

    Update lcmDataFetcher.bat

--- lcmDataFetcher.bat
+++ lcmDataFetcher.bat
@@ -1,3 +1,4 @@
+@echo off
 SET shinyPath=%~dp0
 cd %shinyPath%
-%shinyPath%\dist\R-Portable\App\R-Portable\bin\Rscript.exe %shinyPath%\lcmDataFetcher.R
+%shinyPath%\dist\R-Portable\App\R-Portable\bin\Rscript.exe --no-restore %shinyPath%\lcmDataFetcher.R
