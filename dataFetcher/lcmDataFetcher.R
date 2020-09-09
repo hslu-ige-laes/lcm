2d4710e32be0d58d6661d47b006cd6cfd169ce7a
Reto Marek <reto.marek@hslu.ch>
2020-09-09 10:28:32 +0200

    Create lcmDataFetcher.R

---
+++ dataFetcher/lcmDataFetcher.R
@@ -0,0 +1,41 @@
+
+# Sys.setenv(LC_CTYPE = "en_US.UTF-8")
+
+library(here)
+library(httr)
+library(jsonlite)
+library(lubridate)
+library(tictoc)
+library(readr)
+library(plyr)
+library(tidyverse)
+library(influxdbr)
+library(xts)
+library(highfrequency)
+library(NCmisc)
+
+source(here::here("app", "shiny", "R", "fct_etlSourceTtn_server.R"))
+source(here::here("app", "shiny", "R", "fct_etlSourceInfludxDB_server.R"))
+source(here::here("app", "shiny", "R", "fct_etlSourceCsv_server.R"))
+
+source(here::here("app", "shiny", "R", "fct_etlAggData_server.R"))
+
+source(here::here("app", "shiny", "R", "utils_server.R"))
+
+options(readr.num_columns = 0)
+
+# ======================================================================
+# get newest ttn data from server
+# ======================================================================
+
+tic("ttnFetchServerData")
+ttnFetchServerData()
+toc()
+
+# ======================================================================
+# Aggregator
+# ======================================================================
+
+tic("etlAggFilterData")
+etlAggFilterData()
+toc()
